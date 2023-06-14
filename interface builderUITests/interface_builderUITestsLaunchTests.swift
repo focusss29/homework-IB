77907116938cf8878524f9e70a9a35c77922fbe0
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-06-14 20:13:08 +0600

    Initial Commit

---
+++ interface builderUITests/interface_builderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  interface_builderUITestsLaunchTests.swift
+//  interface builderUITests
+//
+//  Created by Ilya Pogozhev on 14.06.2023.
+//
+
+import XCTest
+
+final class interface_builderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
